881ba4dd6100bc7c7c9ec86886b3858a9b584164
swappy <59965507+rycerzes@users.noreply.github.com>
2024-03-23 20:47:06 +0530

    fix entrypoint.sh

--- ftp_http/src/docker-entrypoint.sh
+++ ftp_http/src/docker-entrypoint.sh
@@ -12,3 +12,11 @@ adduser \
 	-s /bin/false \
 	-u $UID \
 	user
+
+mkdir -p /home/user
+echo "This is a test file" > /home/user/test.txt
+chown -R user:user /home/user
+chmod -R 755 /home/user
+echo "user:123" | /usr/sbin/chpasswd
+
+/usr/bin/supervisord -c /etc/supervisord.conf
