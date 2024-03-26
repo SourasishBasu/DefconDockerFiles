e4fa43e2314f3ef97f48696973d36f5b11ee319b
swappy <59965507+rycerzes@users.noreply.github.com>
2024-03-27 00:15:05 +0530

    modified perms

--- ssh/src/mod-ssh-entrypoint.sh
+++ ssh/src/mod-ssh-entrypoint.sh
@@ -3,10 +3,10 @@
 groupadd -g 2000 bots
 
 useradd -ms /bin/bash ecorp && \
-echo 'ecorp:changethis' | chpasswd --crypt-method=SHA512 && \
-chown -R ecorp:ecorp /home/ecorp && \
-chmod -R 500 /home/ecorp && \
-chmod 700 /home/ecorp
+ echo 'ecorp:changethis' | chpasswd --crypt-method=SHA512 && \
+ chown -R ecorp:ecorp /home/ecorp && \
+ chmod -R 500 /home/ecorp && \
+ chmod 700 /home/ecorp 
 
 mkdir -p /run/sshd && chmod 755 /run/sshd
 
@@ -16,6 +16,7 @@ chmod u+s /usr/bin/su
 chmod 700 /usr/bin/passwd
 chmod 700 /custom
 chmod 700 /mod-ssh-entrypoint.sh
+chmod 500 -R /home/ecorp
 
 python3 custom/acc-gen.py
 python3 custom/pass-gen.py
