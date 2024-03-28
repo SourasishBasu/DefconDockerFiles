8374bf0c11a825c4b8575b3e28a7b08454f88da6
swappy <59965507+rycerzes@users.noreply.github.com>
2024-03-28 13:11:41 +0530

    added wordlists to image

---
+++ ftp_http/src/mod-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+addgroup \
+	-g $GID \
+	-S \
+	user
+
+adduser \
+	-D \
+	-G user \
+	-h /home/user \
+	-s /bin/false \
+	-u $UID \
+	user
+
+mkdir -p /home/user
+echo "This is a test file" > /home/user/test.txt
+cp -r /wordlist/* /home/user/
+chown -R user:user /home/user
+chmod -R 755 /home/user
+echo "user:123" | /usr/sbin/chpasswd
+
+/usr/bin/supervisord -c /etc/supervisord.conf
